cb3f37f8171a4223a8787d91c62228043c9a96ab
Userrr404 <yogeshlilakedev02@gmail.com>
2025-02-21 10:03:25 +0530

    LAB5 Clear

--- LAB4.py
+++ LAB4.py
@@ -1,6 +1,6 @@
 # 1. Accept a name from the user and display that in lower case using lower() function
-# name = input("Enter the name: ")
-# print("Your name in lowercase is: ",name.lower())
+name = input("Enter the name: ")
+print("Your name in lowercase is: ",name.lower())
 
 
 # 2. Write a function that takes one argument and returns 'Positive' if the number is greater than 0, 'Negative' if it's less than 0, and 'Zero' if it's 0. 
