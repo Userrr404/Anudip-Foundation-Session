2554aa97da3443eabceaaf2c33c0847703359e7f
Userrr404 <yogeshlilakedev02@gmail.com>
2025-02-21 12:32:47 +0530

    Modify LAB5 program 1

--- LAB5.py
+++ LAB5.py
@@ -1,15 +1,15 @@
 # 1. Write a python program to reverse a number using a while loop.
-# num = 123
-# reverse = 0
-# while num != 0:
-#     # Get the last digit of the number
-#     rem = num % 10
-#      # Add the digit to the reverse number
-#     reverse = (reverse * 10) + rem
-#     # Remove last element of num & type cast
-#     num =int(num / 10)
+num = 123
+reverse = 0
+while num != 0:
+    # Get the last digit of the number
+    rem = num % 10
+     # Add the digit to the reverse number
+    reverse = (reverse * 10) + rem
+    # Remove last element of num & type cast
+    num =int(num / 10)
 
-# print(reverse)
+print(reverse)
 
 # 2. Write a python program to check whether a number is palindrome or not?
 num = 1001
